43041701023b376bd6a94487d627f3621cf00991
Ramis Damasceno <ramis1998d@gmail.com>
2023-09-05 23:54:37 -0300

    Rotas alteradas para o padrão do next

--- src/pages/Nav.tsx
+++ src/pages/Nav.tsx
@@ -1,18 +1,21 @@
+import Link from "next/link"
+
 export default function Nav() {
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary shadow p-3 mb-5 bg-body-tertiary rounded">
             <div className="container-fluid">
-                <a className="navbar-brand" href="/"><img className="logo" src="logo.png" alt="logo site" /></a>
+                <Link href="/"><p className="navbar-brand"><img className="logo" src="logo.png" alt="logo site" /></p></Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNavDropdown">
                     <ul className="navbar-nav">
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="/">INÍCIO</a>
+                        <Link href="/"><p className="nav-link">INÍCIO</p></Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">SOBRE</a>
+
+                            <Link href="/"><p className="nav-link">SOBRE</p></Link>
                         </li>
                         <li className="nav-item">
                             <a className="nav-link" href="#">CONTATO</a>
