d82fbf55933ab7966709695bfcc77dd738235b93
Ramis Damasceno <ramis.damasceno@youvisa.com.br>
2024-01-11 15:02:06 -0300

    hotjar implementation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,33 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Script
+        id="show-banner"
+        strategy='afterInteractive'
+        dangerouslySetInnerHTML={{
+          __html:
+          `document.getElementById('banner').classList.remove('hidden')
+          (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:3822405,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+          `,
+          
+          
+        }}
+        
+      />
+    </>
+  )
+
 }
